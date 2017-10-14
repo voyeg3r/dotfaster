88e91be833343cc4d0175248d1519fec6923f1d5
voyeg3r <voyeg3r@gmail.com>
2017-10-14 11:03:03 -0300

    removing unicode line separator u-2028 with sed

--- bin/cleanflashcards.sh
+++ bin/cleanflashcards.sh
@@ -3,5 +3,6 @@
 sed -i -r '/^[0-9]+ –/d' output.csv
 sed -i -r 's/<(\/)?(strong|em|br|((p|span|a)[^>]*))(\/)?>//g' output.csv
 sed -i -r 's|(<[/]?)(u)(>)|\1b\3|g' output.csv
-sed -i -r '/^\(/d' output.csv
+sed -i -r '/^(\s+|\t)?\(/d' output.csv
 sed -i -r '/CLIQUE AQUI/,$d' output.csv
+sed -i -r 's/\xE2\x80\xA8//g' output.csv
