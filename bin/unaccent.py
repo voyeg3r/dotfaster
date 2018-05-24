475eabbc08ca8d4729e91765d678411671de6919
voyeg3r <voyeg3r@gmail.com>
2018-05-24 14:27:17 -0300

    updated python unnaccent

--- bin/unaccent.py
+++ bin/unaccent.py
@@ -2,7 +2,7 @@
 # # -*- coding: UTF-8 -*-"
 # ------------------------------------------------
 #         Created:  23-01-2017
-#     Last Change:  2018 mai 24 12:28
+#     Last Change:  2018 mai 24 14:26
 #    What it does:  Remove accents of a given text
 #          Author:  sergio luiz araujo silva
 #            Site:  http://vivaotux.blogspot.com
@@ -13,7 +13,7 @@ import unicodedata
 import sys
 
 argument = sys.argv[1:] if len(sys.argv) > 1 else sys.stdin.read()
-argument = ' '.join(argument).lower()
+argument = ''.join(argument).lower()
 
 def unaccent(text):
     '''
